7577cbef15178e62d5b486bb042d20e66a4673d2
Paul Maxan <paul.maxan@maximuscanada.ca>
2022-08-24 08:51:59 -0300

    Added licensing terms.

--- StellariumServer.cs
+++ StellariumServer.cs
@@ -1,4 +1,24 @@
-﻿using System;
+﻿/*
+ * microp11 2022, Paul Maxan
+ * 
+ * This file is part of the telescope below.
+ * 
+ * The telescope is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the telescope.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+using System;
 using System.Diagnostics;
 using System.Net;
 using System.Net.Sockets;
